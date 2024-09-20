cf0b4bc23aa2a2b463be3f255abb2e6cbf146999
Sarygin Rodion <k1ll4h_tv3t@protonmail.com>
2024-09-21 01:35:04 +0700

    chore: fixed npm task for tailwind + fixed tailwind pathes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["index.html"],
     theme: {
         extend: {
             fontFamily: {
